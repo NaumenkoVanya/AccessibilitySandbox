c7c0362fdc5c48b67e8d865f1998a02cb550709b
Vanya <whitewolf3310@icloud.com>
2024-04-09 13:58:19 +0300

    homework

---
+++ AccessibilitySandbox/AccessibilitySandbox/HomeWork/FixingWordScramble.swift
@@ -0,0 +1,20 @@
+//
+//  FixingWordScramble.swift
+//  AccessibilitySandbox
+//
+//  Created by Ваня Науменко on 9.04.24.
+//
+
+import SwiftUI
+
+struct FixingWordScramble: View {
+    var body: some View {
+        Image("test")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    FixingWordScramble()
+}
